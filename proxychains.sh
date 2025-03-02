afa79ba1cd2d4fa2b57963c48c3122f39d1efc0f
ANTI VIRAL <109937537+tobrutkota@users.noreply.github.com>
2025-03-02 17:18:57 +0800

    Update proxychains.sh

--- proxychains.sh
+++ proxychains.sh
@@ -8,15 +8,17 @@ unzip master.zip && rm master.zip && cd proxychains-ng-master
 mkdir -p ~/.proxychains
 cp ~/proxychains/proxychains-ng-master/src/proxychains.conf ~/.proxychains/proxychains.conf
 
-# Konfigurasi otomatis
+# Bersihin proxy bawaan
+sed -i '/ProxyList/,$d' ~/.proxychains/proxychains.conf
+echo "[ProxyList]" >> ~/.proxychains/proxychains.conf
+
+# Aktifin dynamic_chain + random_chain
 sed -i 's/^#dynamic_chain/dynamic_chain/' ~/.proxychains/proxychains.conf
 sed -i 's/^strict_chain/#strict_chain/' ~/.proxychains/proxychains.conf
-sed -i 's/^random_chain/random_chain/' ~/.proxychains/proxychains.conf
+sed -i 's/^#random_chain/random_chain/' ~/.proxychains/proxychains.conf
 
-# Tambahin semua proxy rotate
+# Tambahin proxy kamu sayang 💕
 cat <<EOF >> ~/.proxychains/proxychains.conf
-
-[ProxyList]
 socks5 38.154.227.167 5868 mnlfbonv oz70qvg3eznw
 socks5 38.153.152.244 9594 mnlfbonv oz70qvg3eznw
 socks5 173.211.0.148 6641 mnlfbonv oz70qvg3eznw
@@ -25,5 +27,8 @@ socks5 64.64.118.149 6732 mnlfbonv oz70qvg3eznw
 socks5 166.88.58.10 5735 mnlfbonv oz70qvg3eznw
 EOF
 
-echo "Proxy Rotate udah aktif, sayangku 😘💕 Sekarang tinggal jalanin!"
+# Tambahin delay biar makin aman
+echo -e "\nsleep 5" >> ~/.proxychains/proxychains.conf
+
+echo "Proxy Rotate + Delay udah aktif, sayangku 😘💕 Sekarang mining-nya auto kabur kayak kamu kabur ke pelukan Poppy 😏✨"
 nano ~/.proxychains/proxychains.conf
