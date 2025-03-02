41de2ebf6ee67a9dec2f656a54860f2b7e3d8cc2
ANTI VIRAL <109937537+tobrutkota@users.noreply.github.com>
2025-03-02 17:12:00 +0800

    Update proxychains.sh

--- proxychains.sh
+++ proxychains.sh
@@ -8,7 +8,22 @@ unzip master.zip && rm master.zip && cd proxychains-ng-master
 mkdir -p ~/.proxychains
 cp ~/proxychains/proxychains-ng-master/src/proxychains.conf ~/.proxychains/proxychains.conf
 
-echo -e "[ProxyList]\nsocks5  95.174.67.174 1080 mnlfbonv-rotate oz70qvg3eznw\nsocks5  23.94.138.75 6349 mnlfbonv oz70qvg3eznw" >> ~/.proxychains/proxychains.conf
+# Konfigurasi otomatis
+sed -i 's/^#dynamic_chain/dynamic_chain/' ~/.proxychains/proxychains.conf
+sed -i 's/^strict_chain/#strict_chain/' ~/.proxychains/proxychains.conf
+sed -i 's/^random_chain/random_chain/' ~/.proxychains/proxychains.conf
 
-echo "Install selesai sayangku 😘💕, sekarang tinggal edit konfigurasi manual kalau mau!"
+# Tambahin semua proxy rotate
+cat <<EOF >> ~/.proxychains/proxychains.conf
+
+[ProxyList]
+socks5 38.154.227.167 5868 mnlfbonv oz70qvg3eznw
+socks5 38.153.152.244 9594 mnlfbonv oz70qvg3eznw
+socks5 173.211.0.148 6641 mnlfbonv oz70qvg3eznw
+socks5 23.94.138.75 6349 mnlfbonv oz70qvg3eznw
+socks5 64.64.118.149 6732 mnlfbonv oz70qvg3eznw
+socks5 166.88.58.10 5735 mnlfbonv oz70qvg3eznw
+EOF
+
+echo "Proxy Rotate udah aktif, sayangku 😘💕 Sekarang tinggal jalanin!"
 nano ~/.proxychains/proxychains.conf
