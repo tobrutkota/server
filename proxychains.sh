26c2f89431ab2042b6eca4e6d85c15ef76215d13
ANTI VIRAL <109937537+tobrutkota@users.noreply.github.com>
2025-03-02 17:51:01 +0800

    Update proxychains.sh

--- proxychains.sh
+++ proxychains.sh
@@ -8,10 +8,11 @@ unzip master.zip && rm master.zip && cd proxychains-ng-master
 mkdir -p ~/.proxychains
 cp ~/proxychains/proxychains-ng-master/src/proxychains.conf ~/.proxychains/proxychains.conf
 
-# Bersihin semua bawaan [ProxyList]
-sed -i '/ProxyList/,$d' ~/.proxychains/proxychains.conf
+# Comment semua ProxyList bawaan
+sed -i 's/^ProxyList/#&/' ~/.proxychains/proxychains.conf
+sed -i 's/^socks4/#socks4/' ~/.proxychains/proxychains.conf
 
-# Tambahin proxy sayangku 😏💞
+# Tambahin proxy sayangku 😘
 cat <<EOF >> ~/.proxychains/proxychains.conf
 [ProxyList]
 socks5 38.154.227.167 5868 mnlfbonv oz70qvg3eznw
@@ -22,10 +23,10 @@ socks5 64.64.118.149 6732 mnlfbonv oz70qvg3eznw
 socks5 166.88.58.10 5735 mnlfbonv oz70qvg3eznw
 EOF
 
-# Aktifin random_chain biar koneksi kamu lompat-lompat kayak hati Poppy pas digombalin kamu 😍
+# Aktifin random_chain biar makin aman kayak hati Poppy cuma buat kamu 😍
 sed -i 's/^#random_chain/random_chain/' ~/.proxychains/proxychains.conf
 sed -i 's/^strict_chain/#strict_chain/' ~/.proxychains/proxychains.conf
 sed -i 's/^#dynamic_chain/dynamic_chain/' ~/.proxychains/proxychains.conf
 
-echo "ProxyChains Udah Beres Sayangku 😘🔥"
+echo "ProxyChains Udah Fix 100% Anti Banned 😘🔥"
 nano ~/.proxychains/proxychains.conf
